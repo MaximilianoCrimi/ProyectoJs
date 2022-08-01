6e10c6f3564f87f452027cfbf8d893323dfebf61
Maxi Crimi <maxicrini@gmail.com>
2022-07-31 21:00:55 -0300

    problemas con el array

--- code.js
+++ code.js
@@ -1,27 +1,45 @@
+class Productos{
 
-const iva = 1.21;
-let final = 0;
-function valorFinal(precio,cuotas) {
-   
-    if (cuotas===2) {
-        final = precio *iva;
-        alert("El precio final sumando el IVA y un 10% de recargo es de: " + final * 1.10 ); 
+    
+    constructor(categoria, subCategoria, marca, presentacion, precio, stock){
+        this.categoria = categoria;
+        this.subCategoria = subCategoria; 
+        this.marca = marca;
+        this.presentacion = presentacion;
+        this.precio = precio;
+        this.stock = stock;
     }
-
-    else if (cuotas===3) {
-        final = precio *iva;
-        alert("El precio final sumando el IVA y un 20% de recargo es de: " + final * 1.20 ); 
-    }
-
-    else if (cuotas===4) {
-        final = precio *iva;
-        alert("El precio final sumando el IVA y un 30% de recargo es de: " + final * 1.30 ); 
-    }else{
-        alert("El precio final en 1 cuota sumando el IVA es de: " + precio * iva)
-    }
+     
+mostrarDescripcion(){
+    
+    return this.categoria +  this.subCategoria + this.marca + this.presentacion + this.precio +this.stock;
+}
 }
 
-let pedirPrecio = parseInt(prompt("Ingrese el precio del producto"));
-let pedirCuotas = parseInt(prompt("Ingrese la cantidad de cuotas. Máximo 4 cuotas.")) 
 
-valorFinal(pedirPrecio,pedirCuotas);
+
+let catalogo = new Array();
+
+catalogo.push(new Productos("BEBIDAS", "GASEOSA", "COCA-COLA", "12 x 2,25 LT", 2500, true));
+catalogo.push(new Productos("BEBIDAS", "GASEOSA", "COCA-COLA", "12 x 1,50 LT", 1800, true));
+catalogo.push(new Productos("BEBIDAS", "CERVEZA", "QUILMES", "12 x 1 LT", 2400, false));
+catalogo.push(new Productos("BEBIDAS", "CERVEZA", "AMSTEL", "12 x 1 LT", 2000, true));
+catalogo.push(new Productos("BEBIDAS", "AGUA", "GLACIAR", "12 x 2LT", 1500, false));
+catalogo.push(new Productos("FIAMBRES", "QUESOS", "SANCOR", "BARRA x 5 KG", 5300, true));
+catalogo.push(new Productos("FIAMBRES", "JAMONES", "PALADINI", "PIEZA x 4 KG", 6200, true));
+catalogo.push(new Productos("FIAMBRES", "SALAMES", "CALCHAQUI", "PIEZA x 3 KG", 4800, true));
+catalogo.push(new Productos("FIAMBRES", "QUESOS", "SERENISIMA", "PIEZA x 5 KG", 5500, false));
+
+let pregunta = prompt("¿Qué producto desea ver? \n 1-BEBIDAS \n 2-FIAMBRES");
+
+if (pregunta =="1") {
+    alert("catalogo: "  + catalogo.join("\n"))
+}
+
+function mostrar(){
+    let mensaje="";
+    for(let i=0; i <= catalogo.length;i++){
+        mensaje+="\n"+catalogo[i].mostrarDescripcion();
+    }
+    return(mensaje);
+}
